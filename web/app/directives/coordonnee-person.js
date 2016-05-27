85b85d4175e87c85903aa28e2f244ce5ca9ecdcd
LeKerss <annas9001@gmail.com>
2016-05-27 11:16:20 +0200

    Added common directive for competencies and hobbies

--- web/app/directives/coordonnee-person.js
+++ web/app/directives/coordonnee-person.js
@@ -16,6 +16,12 @@ angular.module('myResumeApp')
 .directive('xp', function () {
     return {
         restrict: 'E',
-        templateUrl : 'app/views/directives/xp.html'
+        templateUrl: 'app/views/directives/xp.html'
+    }
+})
+.directive('hobcomp', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/views/directives/hobcomp.html'
     }
 });
