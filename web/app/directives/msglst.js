34ebe58d5c3b4d1a44bbc1eb0b41684f9d43a61b
LeKerss <annas9001@gmail.com>
2016-05-29 21:47:36 +0200

    Added messages list + some corrections to the directives for educations

--- web/app/directives/msglst.js
+++ web/app/directives/msglst.js
@@ -4,7 +4,7 @@ angular.module('myResumeApp')
     .directive('message-list', function(){
         return {
             restrict : 'E',
-            templateUrl : 'app/views/directives/message-list.html',
+            templateUrl : 'app/views/directives/msglst.html',
             scope : {
                 coordonnee : '='
             },
